e5d3304d6a97aa3fce6a9da0d9312f7105144aeb
OKUHIRA Shunri <114981884+OkuhiraShunri@users.noreply.github.com>
2024-03-26 21:19:57 +0900

    edit ID.v

--- ID.v
+++ ID.v
@@ -1,6 +1,6 @@
 module ID(
  input CLK, RST,
- input [31:0] Ins,
+ input [31:0] Ins,//命令
  input [31:0] Wdata,
  output reg [31:0] Rdata1,
  output reg [31:0] Rdata2,
@@ -43,14 +43,191 @@ always @(Ins or RST) begin
     else begin
         case(op)
             R_FORM: begin
+                case(funct) //synopsys full_case
+	                ADD: begin
+	                Rdata1 <= REGFILE[rs];
+	                Rdata2 <= REGFILE[rt];
+	                Ed32 <= 32'd0;
+	                end
 
+                    ADDU: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= REGFILE[rt];
+                    Ed32 <= 32'd0;
                     end
-            I_FORM: begin
-                
+
+                    SUB: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= REGFILE[rt];
+                    Ed32 <= 32'd2;
                     end
-            J_FORM: begin
-                
+
+                    SUBU: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= REGFILE[rt];
+                    Ed32 <= 32'd0;
                     end
+
+                    AND: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= REGFILE[rt];
+                    Ed32 <= 32'd0;
+                    end
+
+                    OR: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= REGFILE[rt];
+                    Ed32 <= 32'd0;
+                    end
+
+                    XOR: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= REGFILE[rt];
+                    Ed32 <= 32'd0;
+                    end
+
+                    NOR: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= REGFILE[rt];
+                    Ed32 <= 32'd0;
+                    end
+
+                    SLT: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= REGFILE[rt];
+                    Ed32 <= 32'd0;
+                    end
+
+                    SLTU: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= REGFILE[rt];
+                    Ed32 <= 32'd0;
+                    end
+
+                    SLT: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= REGFILE[rt];
+                    Ed32 <= 32'd0;
+                    end
+
+                    SLTU: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= REGFILE[rt];
+                    Ed32 <= 32'd0;
+                    end
+
+                    SLL: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= shamt;
+                    Ed32 <= 32'd0;
+                    end
+
+                    SRL: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= shamt;
+                    Ed32 <= 32'd0;
+                    end
+
+                    SRA: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= shamt;
+                    Ed32 <= 32'd0;
+                    end
+
+                    SLLV: begin
+                    Rdata1 <= REGFILE[rt];
+                    Rdata2 <= REGFILE[rs];
+                    Ed32 <= 32'd0;
+                    end
+
+                    SRLV: begin
+                    Rdata1 <= REGFILE[rt];
+                    Rdata2 <= REGFILE[rs];
+                    Ed32 <= 32'd0;
+                    end
+
+                    SRAV: begin
+                    Rdata1 <= REGFILE[rt];
+                    Rdata2 <= REGFILE[rs];
+                    Ed32 <= 32'd0;
+                    end
+
+                    MFHI: begin
+                    Rdata1 <= 32'd0;
+                    Rdata2 <= 32'd0;
+                    Ed32 <= 32'd0;
+                    end
+
+                    MTHI: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= 32'd0;
+                    Ed32 <= 32'd0;
+                    end
+
+                    MFLO: begin
+                    Rdata1 <= 32'd0;
+                    Rdata2 <= 32'd0;
+                    Ed32 <= 32'd0;
+                    end
+
+                    MTLO: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= 32'd0;
+                    Ed32 <= 32'd0;
+                    end
+
+                    MULT: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= REGFILE[rt];
+                    Ed32 <= 32'd0;
+                    end
+
+                    MULTU: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= REGFILE[rt];
+                    Ed32 <= 32'd0;
+                    end
+
+                    DIV: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= REGFILE[rt];
+                    Ed32 <= 32'd0;
+                    end
+
+                    DIVU: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= REGFILE[rt];
+                    Ed32 <= 32'd0;
+                    end
+
+                    JR: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= 32'd0;
+                    Ed32 <= 32'd0;
+                    end
+
+                    JALR: begin
+                    Rdata1 <= REGFILE[rs];
+                    Rdata2 <= 32'd0;
+                    Ed32 <= 32'd0;
+                    end
+
+                default: begin
+                    Rdata1 <= Rdata1;
+                    Rdata2 <= Rdata2;
+                    Ed32 <= Ed32;
+                    end
+                endcase
+                end//R形式ここまででおしまい
+
+            ADDI: begin
+                Rdata1 <= REGFILE[rs];
+                Rdata2 <= 32'd0;
+                if(Ins[15] == 1'd0) 
+                    Ed32 <= {16'd0,offset_immd};
+                else if (Ins[15] == 1'd1)
+                    Ed32 <= {16'b1111111111111111,offset_immd};
+                end
         endcase
     end 
 end
